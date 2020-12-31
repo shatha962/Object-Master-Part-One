62c446a0a4d7d1dd23951b10ce437aadfdd28187
Shatha96 <shathabast62@gmail.com>
2020-12-31 15:41:02 +0200

    first commit

---
+++ src/com/ObjectMaster/Human.java
@@ -0,0 +1,16 @@
+package com.ObjectMaster;
+
+public class Human {
+    private int strength = 0;
+    private int stealth = 0;
+    private int intelligence = 0;
+    private int health = 0;
+
+    public Human(){
+        this.health = 100;
+        this.intelligence = 3;
+        this.stealth = 3;
+        this.strength = 3;
+    }
+    
+}
